e5cae2f7a59e7263f0453592b930ea7db8549cd1
Justin Lee <justinjameslee5@gmail.com>
2021-08-21 16:51:26 +1000

    Dashboard v1
    - Dashboard
    - Chart
    - Logged In Page

--- app.py
+++ app.py
@@ -16,43 +16,15 @@ def pose():
 @app.route('/login.html')
 def login():
     return render_template('login.html')
+
+@app.route('/dashboard.html')
+def dashboard():
+    return render_template('dashboard.html')
   
 @app.route('/')
 def index():
     return render_template('index.html')
 
-def gen():
-    # Get webcam video
-    cap = cv2.VideoCapture(0)
-
-    #Set dimensions
-    cap.set(3, 1920)
-    cap.set(4, 1080)
-
-    with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
-        while cap.isOpened():
-            ret, frame = cap.read()
-            
-            # Recolor image to RGB
-            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            image.flags.writeable = False
-        
-            # Make detection
-            results = pose.process(image)
-        
-            # Recolor back to BGR
-            image.flags.writeable = True
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-            
-            # Render detections
-            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                    mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
-                                    mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
-                                    )     
-
-            ret,jpg=cv2.imencode('.jpg',image)
-            yield(b'--frame\r\n'b'Content-Type:  image/jpeg\r\n\r\n' + jpg.tobytes() + b'\r\n\r\n')
-        
 @app.route('/video')
 def video():
     return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
@@ -67,14 +39,13 @@ def calculate_angle(a,b,c):
     
     return angle
 
-
 def gen():
     # Get webcam video
     cap = cv2.VideoCapture(0)
 
     #Set dimensions
-    cap.set(3, 1920)
-    cap.set(4, 1080)
+    cap.set(3, 1280)
+    cap.set(4, 720)
     # Curl counter variables
     counter = 0 
     stage = None
@@ -178,4 +149,4 @@ def gen():
             ret,jpg=cv2.imencode('.jpg',image)
             yield(b'--frame\r\n'b'Content-Type:  image/jpeg\r\n\r\n' + jpg.tobytes() + b'\r\n\r\n')
 
-app.run(debug=True)
+app.run(debug='true')
